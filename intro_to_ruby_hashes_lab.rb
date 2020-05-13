1b07c3ea9e6f0be6b4d526d7c6bae5cb59dfadb4
Travis Prol <travisjohnprol@gmail.com>
2020-05-13 12:43:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,8 @@
 def base_hash
   monolopy = {:railroads => {
-    
+    :pieces => 4,
+    :rent_in_dollars => {},
+    :names => {}
   }}
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
